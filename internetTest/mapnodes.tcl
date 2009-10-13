46a41cb1a32831c0f198b4a0755621ff9bfdaba2
sdpalme <sdpalme@9598f20a-a955-11de-bea2-1db044c32224>
2009-10-13 21:43:19 +0000

    running for simple example

--- internetTest/mapnodes.tcl
+++ internetTest/mapnodes.tcl
@@ -1,46 +1,6 @@
-for {set i 0} {$i < 5} {incr i} {
+for {set i 0} {$i < 6} {incr i} {
 	for {set j 0} {$j < 1} {incr j} {
 		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng0_" $j]
-		set tcp0 [new Agent/TCP/FullTcp]
-		set dummy [new MRPerf/NodeApp $tcp0]
-		eval "$dummy set hnode $mn"
-		set app11 [$dummy new-connection $jt]
-		$ns at 0.05 "$app11 send_heartbeat"
-	}
-	for {set j 0} {$j < 1} {incr j} {
-		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng1_" $j]
-		set tcp0 [new Agent/TCP/FullTcp]
-		set dummy [new MRPerf/NodeApp $tcp0]
-		eval "$dummy set hnode $mn"
-		set app11 [$dummy new-connection $jt]
-		$ns at 0.05 "$app11 send_heartbeat"
-	}
-	for {set j 0} {$j < 1} {incr j} {
-		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng2_" $j]
-		set tcp0 [new Agent/TCP/FullTcp]
-		set dummy [new MRPerf/NodeApp $tcp0]
-		eval "$dummy set hnode $mn"
-		set app11 [$dummy new-connection $jt]
-		$ns at 0.05 "$app11 send_heartbeat"
-	}
-	for {set j 0} {$j < 1} {incr j} {
-		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng3_" $j]
-		set tcp0 [new Agent/TCP/FullTcp]
-		set dummy [new MRPerf/NodeApp $tcp0]
-		eval "$dummy set hnode $mn"
-		set app11 [$dummy new-connection $jt]
-		$ns at 0.05 "$app11 send_heartbeat"
-	}
-	for {set j 0} {$j < 1} {incr j} {
-		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng4_" $j]
-		set tcp0 [new Agent/TCP/FullTcp]
-		set dummy [new MRPerf/NodeApp $tcp0]
-		eval "$dummy set hnode $mn"
-		set app11 [$dummy new-connection $jt]
-		$ns at 0.05 "$app11 send_heartbeat"
-	}
-	for {set j 0} {$j < 1} {incr j} {
-		set mn [format "%s%s%s%s" "\$n_rg0_" $i "_ng5_" $j]
 		set tcp0 [new Agent/TCP/FullTcp]
 		set dummy [new MRPerf/NodeApp $tcp0]
 		eval "$dummy set hnode $mn"
